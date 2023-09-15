8cbce2fe8e2e0e5bb0ca2af9c7f496eef52ff433
Mark Ruzicka <markfruzicka@gmail.com>
2023-09-15 08:57:26 -0500

    does not currently work

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Timer from './Timer';
+import NewQuote from './QuoteAPI';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [restart, setRestart] = useState(false);
   const [timerSeconds, setTimerSeconds] = useState(0);
-  const [quote, setQuote] = useState("");
+  const [getQuote, setGetQuote] = useState(true);
+  const quote = " "
   const [wpm, setWPM] = useState(0);
 
-  // make this check the characters are correct and must update their class(correct, wrong, from unentered)
-  useEffect(() => {
-    
-  }, [inputValue]);
-
-// return a span for each letter in the quote
-const formatingQuote = () => {
-
-};
-  
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
-    //makes sure the time is only started once when the first char is entered, updates bools
     if (restart == false) {
       setIsTimerRunning(true);
       setRestart(true);
     }
+    
     if(isTimerRunning && (event.target.value.length===(JSON.stringify(quote).length)-6)) {
-    //stops the test once the length of both the inputed value and the quote are the same length
       setIsTimerRunning(false);
       let arr = JSON.stringify(quote).split(" ");
       let arr2 = event.target.value.split(" ");
@@ -43,41 +34,21 @@ const formatingQuote = () => {
     }
   };
 
-  //updates the seconds being displayed when the useEffect in the timer files calls this function
   const handleSecondsChange = (seconds) => {
     if (seconds !== timerSeconds) {
       setTimerSeconds(seconds);
     }
   };
 
-  // move this to another file, make sure it returns a stirng in the right format
-  const fetchQuote = () => {
-    fetch('https://api.quotable.io/quotes/random')
-      .then((res) => res.json())
-      .then((data) => setQuote(data.map(item => JSON.stringify(item.content))))
-      .catch((er) => {
-        console.log('error fetching quote: ', er);
-      });
-  };
-
   const genNew = () => {
     setRestart(false);
     setIsTimerRunning(false);
     setInputValue("");
-    fetchQuote();
   }
-
-  useEffect(() => {
-     if (quote == "") {
-       console.log("here")
-      fetchQuote();
-     }
-  }, []);
-
 
   return (
     <div>
-      <div>{quote}</div>
+      <NewQuote getQuote={getQuote} setGetQuote={setGetQuote}/>
       <input id='inputbox' value={inputValue} onChange={handleInputChange} />
       <button onClick={genNew}> Restart </button>
       <Timer isRunning={isTimerRunning} onSecondsChange={handleSecondsChange} restart={restart} />
--- src/QuoteAPI.js
+++ src/QuoteAPI.js
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
-function NewQuote({ setString }) {
-  useEffect(() => {
-    fetchQuote();
-  }, []);
-
+function NewQuote({ getQuote, setGetQuote }) {
   const fetchQuote = () => {
     fetch('https://api.quotable.io/quotes/random')
       .then((res) => res.json())
       .then((data) => {
-        const quote = data.content;
-        setString(quote);
+        setGetQuote(false)
+        console.log("here")
+        setQuote(data.map(item => JSON.stringify(item.content)));
       })
       .catch((er) => {
         console.log('error fetching quote: ', er);
       });
   };
 
-  return null; // or return some JSX if needed
+  const [quote, setQuote] = useState(fetchQuote)
+
+  return quote;// or return some JSX if needed
 }
 
 export default NewQuote;
